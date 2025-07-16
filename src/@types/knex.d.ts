d58597235f8cb7cb5edb80d447a4237c703b5d6a
PauloStraforini <paulostraforini@gmail.com>
2025-07-15 22:14:42 -0300

    chore: add user routes with CRUD operations and update knex types for users table

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// eslint-disable-next-line prettier/prettier
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    users: {
+      id: string
+      name: string
+      email: string
+      created_at: string
+      updated_at: string
+      session_id?: string
+    }
+  }
+}
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,14 +1,40 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { knex } from '../database'
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const users = await knex('users').where('id', 1).select('*')
-    return users
+  app.get('/', async () => {
+    const users = await knex('users').select('*')
+    return { users }
   })
 
-  app.get('/users', async () => {
-    const users = await knex('users').select('*')
-    return users
+  app.get('/:id', async (request) => {
+    const getUserParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getUserParamsSchema.parse(request.params)
+
+    const user = await knex('users').where({ id }).first()
+
+    return { user }
+  })
+
+  app.post('/', async (request, reply) => {
+    const createUserBodySchema = z.object({
+      name: z.string(),
+      email: z.string().email(),
+    })
+
+    const { name, email } = createUserBodySchema.parse(request.body)
+
+    await knex('users').insert({
+      id: crypto.randomUUID(),
+      name,
+      email,
+      session_id: crypto.randomUUID(),
+    })
+
+    return reply.status(201).send()
   })
 }
