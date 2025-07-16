c99891068114ddfa596e84a42131d68ccce5c25d
Straforini <134074601+PauloStraforini@users.noreply.github.com>
2025-07-16 11:07:35 -0300

    chore: implement meals route with POST method and validation schema

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -12,5 +12,16 @@ declare module 'knex/types/tables' {
       updated_at: string
       session_id?: string
     }
+
+    meals: {
+      id: string
+      user_id: string
+      name: string
+      description: string
+      is_on_diet: boolean
+      date: number // unix timestamp
+      created_at: string
+      updated_at: string
+    }
   }
 }
---
+++ src/routes/meals.ts
@@ -0,0 +1,30 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
+import { knex } from '../database'
+
+export default function mealsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createMealBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      is_diet: z.boolean(),
+      date: z.string().datetime(),
+    })
+
+    const { name, description, isOnDiet, date } = createMealBodySchema.parse(
+      request.body,
+    )
+
+    await knex('meals').insert({
+      id: randomUUID(),
+      name,
+      description,
+      is_on_diet: isOnDiet,
+      date: date.getTime(),
+      user_id: request.user?.id,
+    })
+
+    reply.status(201).send()
+  })
+}
